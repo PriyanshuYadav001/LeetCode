deae952a49c0c2160234781d28ba3aa11ada4957
PriyanshuYadav001 <152767612+PriyanshuYadav001@users.noreply.github.com>
2025-05-07 00:14:49 +0530

    Initial push

--- 1128.number-of-equivalent-domino-pairs.cpp
+++ 1128.number-of-equivalent-domino-pairs.cpp
@@ -5,10 +5,18 @@
  */
 
 // @lc code=start
+#include <map>
 class Solution {
 public:
     int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        
+        for(auto &d:dominoes) sort(d.begin(),d.end());
+        unordered_map<int,int> mp;
+        for(auto &d:dominoes) mp[d[0]*10+d[1]]++;
+        int res=0;
+        for(auto m:mp){
+            res+=m.second*(m.second-1)/2;
+        }
+        return res;
     }
 };
 // @lc code=end
---
+++ 1920.build-array-from-permutation.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1920 lang=cpp
+ *
+ * [1920] Build Array from Permutation
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
