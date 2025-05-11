abbcb8e0148d1700ed7ad48859512e7215338a51
Priyanshu Yadav <152767612+PriyanshuYadav001@users.noreply.github.com>
2025-05-11 13:45:50 +0530

    Update 1550.three-consecutive-odds.cpp

--- 1550.three-consecutive-odds.cpp
+++ 1550.three-consecutive-odds.cpp
@@ -8,9 +8,10 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        if(arr.size()<3) return false;
-        for(int i=0;i<arr.size()-2;i++){
-            if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1) return true;
+        if(arr.size()>=3){ 
+            for(int i=0;i<arr.size()-2;i++){
+                if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1) return true;
+            }
         }
         return false;
     }
