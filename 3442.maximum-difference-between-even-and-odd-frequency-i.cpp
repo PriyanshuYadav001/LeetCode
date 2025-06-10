497c9551bea3bff972bed22c3524db33aadd591a
PriyanshuYadav001 <152767612+PriyanshuYadav001@users.noreply.github.com>
2025-06-10 22:21:41 +0530

    todays ques.

---
+++ 3442.maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=3442 lang=cpp
+ *
+ * [3442] Maximum Difference Between Even and Odd Frequency I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char, int> mp;
+        int odd = 0;
+        int even = INT_MAX;
+        for (char c : s)
+            mp[c]++;
+        for (auto m : mp) {
+            if (m.second % 2 == 0)
+                even = min(even, m.second);
+            else
+                odd = max(odd, m.second);
+        }
+        return odd - even;
+    }
+};
+// @lc code=end
+
